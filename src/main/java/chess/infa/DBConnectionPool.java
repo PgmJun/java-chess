f2357fb94a3246527c7f282cc4f1b44c3a6982f9
PgmJun <chltmdwns96@gmail.com>
2024-03-29 22:20:52 +0900

    feat: DB 연결

--- src/main/java/chess/infa/DBConnectionPool.java
+++ src/main/java/chess/infa/DBConnectionPool.java
@@ -10,9 +10,9 @@ public class DBConnectionPool {
 
     private static final String SERVER = "localhost:13306"; // MySQL 서버 주소
     private static final String DATABASE = "chess"; // MySQL DATABASE 이름
-    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=KTC";
-    private static final String USERNAME = "root"; //  MySQL 서버 아이디
-    private static final String PASSWORD = "root"; // MySQL 서버 비밀번호
+    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String USERNAME = "chess"; //  MySQL 서버 아이디
+    private static final String PASSWORD = "chess"; // MySQL 서버 비밀번호
 
     private static final int MAX_CONNECTION_SIZE = 3;
 
