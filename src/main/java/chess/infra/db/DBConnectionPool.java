25b534444b897fa8fcffb208a48bc773af243b4f
PgmJun <chltmdwns96@gmail.com>
2024-04-02 13:40:15 +0900

    refactor: 커넥션 생성기, 커넥션 풀 역할 분리

--- src/main/java/chess/infra/db/DBConnectionPool.java
+++ src/main/java/chess/infra/db/DBConnectionPool.java
@@ -1,28 +1,19 @@
 package chess.infra.db;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class DBConnectionPool {
-
-    private static final String SERVER = "localhost:13306"; // MySQL 서버 주소
-    private static final String DATABASE = "chess"; // MySQL DATABASE 이름
-    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USERNAME = "chess"; //  MySQL 서버 아이디
-    private static final String PASSWORD = "chess"; // MySQL 서버 비밀번호
     private static final int MAX_CONNECTION_SIZE = 3;
-
     private static final Deque<Connection> CONNECTION_POOL;
 
     static {
         CONNECTION_POOL = new ArrayDeque<>();
         for (int i = 0; i < MAX_CONNECTION_SIZE; i++) {
             try {
-                Connection connection = DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
-                CONNECTION_POOL.add(connection);
+                CONNECTION_POOL.add(DBConnectionGenerator.generate());
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
